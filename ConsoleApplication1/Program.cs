5e44df5cb408e286f37c04b424fff47ade4fae09
evodean0 <evodean0@EVH01920NB>
2016-04-26 12:08:19 +0200

    Fixed Experiment.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace Experiment
+namespace Nancy.Simple
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            string JSonString = System.IO.File.ReadAllText("testjson.txt");
+            Console.Read();
         }
     }
 }
