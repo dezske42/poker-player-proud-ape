d28f9a52b6780256adca3ee2d23360298b771333
evodean0 <evodean0@EVH01920NB>
2016-04-26 16:00:24 +0200

    ranking service rough access

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
             string JSonString = System.IO.File.ReadAllText("testjson.txt");
             JObject jobj = JObject.Parse(JSonString);
-            Console.WriteLine(jobj.GetValue("minimum_raise"));
+            Poker poker = new Poker(jobj);
+            Console.WriteLine("http://rainman.leanpoker.org/rank?cards=" + poker.AllCardsJSon);
+
+
+            System.Net.WebRequest request;
+            request = WebRequest.Create("http://rainman.leanpoker.org/rank?cards="+poker.AllCardsJSon);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream resStream = response.GetResponseStream();
+            StreamReader resStreamReader = new StreamReader(resStream);
+            String streamString = resStreamReader.ReadToEnd();
+            Console.WriteLine(streamString);
             Console.Read();
         }
     }
