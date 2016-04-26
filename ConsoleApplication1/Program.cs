9db907ea51a8a8511759b2969295c1e1f44c9e66
evodean0 <evodean0@EVH01920NB>
2016-04-26 12:12:04 +0200

    Added test parsing.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
@@ -13,6 +13,8 @@ namespace Nancy.Simple
         {
             Console.WriteLine("Hello");
             string JSonString = System.IO.File.ReadAllText("testjson.txt");
+            JObject jobj = JObject.Parse(JSonString);
+            Console.WriteLine(jobj.GetValue("minimum_raise"));
             Console.Read();
         }
     }
